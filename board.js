6ffe986f285364e92d62066ac979bd3f571fb56b
Zsolt Jakab <zsolt_jakab@epam.com>
2017-03-14 19:27:19 +0100

    using mutation observer instead of dom manipulation

--- board.js
+++ board.js
@@ -1,7 +1,7 @@
 "use strict";
 
 $(document).ready(function() {
-  
+  var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
   var storage = new Storage("sample", localStorage);
   storage.loadBoard();
 
@@ -11,12 +11,22 @@ $(document).ready(function() {
   
 
   $('#sample').on("DOMSubtreeModified", function(e){
-    storage.saveBoard();
-    console.log("happening");
+    //storage.saveBoard();
+    //console.log("happening");
   });  
   
-    $('#sample').mouseup( function(e){
-      //storage.saveBoard();
-      //console.log("happening");
-  });  
+  
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      storage.saveBoard();
+      console.log("happening");
+    });
+  });
+
+  observer.observe(document, {
+    attributes: true,
+    childList: true,
+    subtree: true,
+    characterData: true
+  });
 });
